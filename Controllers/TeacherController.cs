39b776fde0d954cbe20561812bc5458c6bcdbbc6
N <N@DESKTOP-5EDURD5>
2024-03-22 22:05:18 -0400

    Add project files.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,29 @@
+﻿using Cumulative_Project_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cumulative_Project_1.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        // GET: Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.findTeacher(id);
+
+            return View(NewTeacher);
+        }
+    }
+}
